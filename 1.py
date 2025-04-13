0b726159e389d2009adabcb01e6afc4178335e07
Sarthak Pathak <sarthakpathak1608@gmail.com>
2025-04-13 14:34:53 +0530

    python questions

---
+++ 1.py
@@ -0,0 +1,6 @@
+# calculate the area of the rectangle
+length = float(input("enter the length of the rectangle"))
+breadth = float(input("enter the breadth of the rectangle"))
+area = length*breadth
+print("area of the rectangle is ", area, "units²")
+# superscript  can be used by clicking the key: num lock, alt+0178
---
+++ 4.py
@@ -0,0 +1,6 @@
+"""Write a Python code to iterate the first 10 numbers, and in each iteration,
+print the sum of the current and previous number"""
+
+for i in range(0,10):
+    print("Current Number", i ,"Previous Number",i-1,"Sum:",i+(i-1))
+          
