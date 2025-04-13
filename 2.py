8e701a9964921b1a200f2b304a833be008bd65fc
Sarthak Pathak <134594305+Sarthak16082004@users.noreply.github.com>
2025-04-13 15:05:52 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,8 +0,0 @@
-#shopping cart
-item=input("what item would you like to buy?")
-quantity=int (input("how much item you you like to buy?"))
-cost =float(input("what is the cost of per piece item"))
-total=quantity*cost
-
-print("you had buyed :",item)
-print("total cost of",item,"is :",total)
